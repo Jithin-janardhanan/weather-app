aa8f09c67ccd434a8a759ff3d8ea0bb224ac7923
Jithin-janardhanan <jithinjanardhanan97@gmail.com>
2025-04-09 22:00:14 +0530

    started ui

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/constant.dart' as k;
 
@@ -12,6 +12,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isLoaded = false;
+  // late num temp;
+  // num hum;
+  // num hum;
+  // num cover;
+  late String cityname;
+  TextEditingController controller = TextEditingController();
+
   @override
   void initState() {
     // TODO: implement initState
@@ -21,7 +29,81 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 33, 120, 71),
+            Color.fromARGB(255, 62, 1, 66),
+          ], begin: Alignment.bottomLeft, end: Alignment.topRight)),
+          child: Visibility(
+            visible: isLoaded,
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: MediaQuery.of(context).size.height * 0.09,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 236, 234, 234)
+                        .withOpacity(0.3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        20,
+                      ),
+                    ),
+                  ),
+                  child: Center(
+                    child: TextFormField(
+                      onFieldSubmitted: (String s) {
+                        setState(() {
+                          cityname = s;
+                          getCitywether(s);
+                          isLoaded = false;
+                        });
+                      },
+                      controller: controller,
+                      cursorColor: Colors.white,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Search city',
+                        hintStyle: TextStyle(fontSize: 15, color: Colors.white),
+                        prefixIcon: Icon(
+                          Icons.search_rounded,
+                          size: 25,
+                          color: Colors.white,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(),
+                )
+              ],
+            ),
+            replacement: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   getCurrentLocation() async {
@@ -38,6 +120,22 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  getCitywether(String cityname) async {
+    var client = http.Client();
+    var uri = '${k.domain}q=$cityname&appid=${k.apiKey}';
+    var url = Uri.parse(uri);
+    print(url);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodeData = json.decode(data);
+      print(data);
+      updateUI(decodeData);
+    } else {
+      (response.statusCode);
+    }
+  }
+
   getCurrentCityWeather(Position position) async {
     var client = http.Client();
     var uri =
@@ -47,8 +145,16 @@ class _HomeScreenState extends State<HomeScreen> {
     var response = await client.get(url);
     if (response.statusCode == 200) {
       var data = response.body;
+      var decodeData = json.decode(data);
       print(data);
-    } else
+      updateUI(decodeData);
+      setState(() {
+        isLoaded = true;
+      });
+    } else {
       (response.statusCode);
+    }
   }
+
+  updateUI(var decodedData) {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/home_screen.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomeScreen(),
+    theme: ThemeData(
+        primaryColor: Colors.white,
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.white)),
   ));
 }
-
